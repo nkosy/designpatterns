9d889fa6702517bebd135f6362ceb2505a490ab1
nkosy <nkosy@nkosy-Presario-CQ57-Notebook-PC>
2014-05-27 05:31:16 +0200

    On chain responsibility

--- src/main/java/com/nkosy/designpatterns/behavioral/MercuryHandler.java
+++ src/main/java/com/nkosy/designpatterns/behavioral/MercuryHandler.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.nkosy.designpatterns.behavioral;
 
 /**
@@ -11,18 +10,22 @@ package com.nkosy.designpatterns.behavioral;
  * @author nkosy
  */
 public class MercuryHandler extends PlanetHandler {
-    
-    public void handleRequest(PlanetEnum request){
-        if(request == PlanetEnum.MERCURY) {
+
+    public void handleRequest(PlanetEnum request) {
+        if (request == PlanetEnum.MERCURY) {
             System.out.println("MercuryHandler handles " + request);
             System.out.println("Mercury is hot.\n");
-        }
-        else{
+        } else {
             System.out.println("MercuryHandler doesnt handle " + request);
-            /*(if(successor != null){
-                successor.handleRequest(request);
-            }*/
+            if (successor != null) {
+                successor.handlerequest(request);
+            }
         }
     }
-    
+
+    @Override
+    public void handlerequest(PlanetEnum request) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
+
